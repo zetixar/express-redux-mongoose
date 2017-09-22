152e4037cce1c75e9ca1078a8e75bac898e4900d
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2017-09-22 12:46:37 -0400

    Fix default testes for Header

--- client/src/tests/CounterContainer.test.js
+++ client/src/tests/CounterContainer.test.js
@@ -1,8 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { shallow } from 'enzyme'
+
 import { CounterContainer } from 'containers/CounterContainer'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<CounterContainer />, div)
+  ReactDOM.render(
+    <CounterContainer
+      increment={() => {}}
+      decrement={() => {}}
+      incrementIfOdd={() => {}}
+      counter={0}
+    />,
+    div,
+  )
+})
+
+it('renders consistantly[snapshot] without crashing', () => {
+  const wrapper = shallow(
+    <CounterContainer
+      increment={() => {}}
+      decrement={() => {}}
+      incrementIfOdd={() => {}}
+      counter={0}
+    />,
+  )
+  expect(wrapper).toMatchSnapshot()
 })
--- client/src/tests/Header.test.js
+++ client/src/tests/Header.test.js
@@ -1,8 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { shallow } from 'enzyme'
+
 import { Header } from 'containers/Header'
 
-it('2 + 2 = 4', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<Header />, div)
+jest.mock('react-router')
+
+it('renders without crashing', () => {
+  const context = { router: { isActive: () => true } }
+  const renderedComponent = shallow(
+    <Header to="/home" logout={() => {}} history={{}} />,
+    { context },
+  )
+
+  expect(renderedComponent.hasClass('active')).toEqual(false)
+})
+
+it('renders consistantly[snapshot] without crashing', () => {
+  const wrapper = shallow(<Header logout={() => {}} history={{}} />)
+  expect(wrapper).toMatchSnapshot()
 })
